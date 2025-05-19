67ce7a336efe07798ec81df6cb5e975f378d20e5
oshosh <dhtpgus7@gmail.com>
2025-05-20 00:48:45 +0900

    fix: 순환 참조 해결결

--- hooks/useHotKeys/constants.ts
+++ hooks/useHotKeys/constants.ts
@@ -1,84 +1,10 @@
+import { keyNameCodeMapping as keyMap, modifierNameKeyCodeMapping as modifierMap } from './keyMaps';
 import { ChooseModifiersType, KeyName, ModifierName, ModifiersUnionType } from './types';
 
-/**
- * 현재 지원되는 키보드 key name을 readonly로 매핑합니다.
- */
-export const keyNameCodeMapping: Readonly<Record<KeyName, number>> = {
-  backspace: 8,
-  tab: 9,
-  clear: 12,
-  enter: 13,
-  return: 13,
-  esc: 27,
-  escape: 27,
-  space: 32,
-  left: 37,
-  up: 38,
-  right: 39,
-  down: 40,
-  del: 46,
-  delete: 46,
-  ins: 45,
-  insert: 45,
-  home: 36,
-  end: 35,
-  pageup: 33,
-  pagedown: 34,
-  capslock: 20,
-  num_0: 96,
-  num_1: 97,
-  num_2: 98,
-  num_3: 99,
-  num_4: 100,
-  num_5: 101,
-  num_6: 102,
-  num_7: 103,
-  num_8: 104,
-  num_9: 105,
-  num_multiply: 106,
-  num_add: 107,
-  num_enter: 108,
-  num_subtract: 109,
-  num_decimal: 110,
-  num_divide: 111,
-  '⇪': 20,
-  ',': 188,
-  '.': 190,
-  '/': 191,
-  '`': 192,
-  '-': 189,
-  '=': 187,
-  ';': 186,
-  "'": 222,
-  '[': 219,
-  ']': 221,
-  '\\': 220,
-};
+export const keyNameCodeMapping: Readonly<Record<KeyName, number>> = keyMap;
 
-/**
- * 현재 지원되는 수정자 키를 전부 readOnly로 매핑 합니다.
- */
-export const modifierNameKeyCodeMapping: Readonly<Record<ModifierName, number>> = {
-  // shiftKey
-  '⇧': 16,
-  shift: 16,
-  // altKey
-  '⌥': 18,
-  alt: 18,
-  option: 18,
-  // ctrlKey
-  '⌃': 17,
-  ctrl: 17,
-  control: 17,
-  // metaKey
-  '⌘': 91,
-  cmd: 91,
-  command: 91,
-};
+export const modifierNameKeyCodeMapping: Readonly<Record<ModifierName, number>> = modifierMap;
 
-/**
- * 현재 조합 할 수 있는 수정자 키를 상수로 선언합니다.
- */
 export const modifiers: ChooseModifiersType<ModifiersUnionType> = [
   'command',
   'shift',
--- hooks/useHotKeys/types.ts
+++ hooks/useHotKeys/types.ts
@@ -1,39 +1,17 @@
-import { keyNameCodeMapping, modifierNameKeyCodeMapping } from './constants';
+import { keyNameCodeMapping, modifierNameKeyCodeMapping } from './keyMaps';
 
 export type KeyName = keyof typeof keyNameCodeMapping;
 export type ModifierName = keyof typeof modifierNameKeyCodeMapping;
 
-/**
- * Hook에서 지원하는 Hook키의 interface를 정의 합니다.
- */
 export type Hotkey = {
-  /**
-   * 일치하는 핫키를 누를때 callback이 트리거 되며 실행됩니다.
-   */
   callback: (e: KeyboardEvent) => void;
-  /**
-   * shortcuts를 정의 하며 multi의 대체 키 리스로도 사용이 가능합니다.
-   */
   match: string[] | string;
-  /**
-   * 텍스트 입력에 초점이 맞춰져 있는 동안 핫키가 실행되도록 허용합니다.
-   */
   includeInputs?: boolean;
-  /**
-   * keydown 이벤트에서 PreventDefault를 호출하지 마세요.
-   * 기본적으로 브라우저 이벤트 보다 더 상위로 동작 합니다.
-   */
   skipPreventDefault?: boolean;
 };
 
-/**
- * 수정자 UnionType를 정의 합니다.
- */
 export type ModifiersUnionType = 'command' | 'shift' | 'ctrl' | 'alt';
 
-/**
- * 수정자는 한정된 컨디셔널 타입을 반환합니다.
- */
 export type ChooseModifiersType<A> = A extends ModifiersUnionType
   ? Array<ModifiersUnionType>
   : never;
