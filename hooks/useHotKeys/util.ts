65f7e4e67cf9c64db559cd5ab656b37cc2083c34
oshosh <dhtpgus7@gmail.com>
2025-05-20 00:52:29 +0900

    fix: 타입 가드 함수 추가

--- hooks/useHotKeys/util.ts
+++ hooks/useHotKeys/util.ts
@@ -1,8 +1,31 @@
 import { keyNameCodeMapping, modifierNameKeyCodeMapping } from './constants';
+import { KeyName, ModifierName } from './types';
 
 export default function toArray<T>(val: T | T[]) {
   return Array.isArray(val) ? val : [val];
 }
+
+function isKeyName(key: string): key is KeyName {
+  return key in keyNameCodeMapping;
+}
+
+function isModifierName(key: string): key is ModifierName {
+  return key in modifierNameKeyCodeMapping;
+}
+
+export const getKeyCode = (x: string): number => {
+  const lowerKey = x.toLowerCase();
+
+  if (isKeyName(lowerKey)) {
+    return keyNameCodeMapping[lowerKey];
+  }
+
+  if (isModifierName(lowerKey)) {
+    return modifierNameKeyCodeMapping[lowerKey];
+  }
+
+  return x.toUpperCase().charCodeAt(0);
+};
 
 export const isKeyPressed = (key: string, evt: KeyboardEvent): boolean => {
   const keyCode = getKeyCode(key);
@@ -19,8 +42,3 @@ export const isKeyPressed = (key: string, evt: KeyboardEvent): boolean => {
       return keyCode === evt.keyCode;
   }
 };
-
-export const getKeyCode = (x: string): number =>
-  keyNameCodeMapping[x.toLowerCase()] ||
-  modifierNameKeyCodeMapping[x.toLowerCase()] ||
-  x.toUpperCase().charCodeAt(0);
