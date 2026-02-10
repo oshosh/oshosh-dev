40f8b7b0864b3a7cab441dc8b4c1f90a923f458e
DESKTOP-7BOAEJ7\KBDS_ds <dhtpgus7@gmail.com>
2026-02-10 16:28:26 +0900

    fix: 줌임 딤드 추가

--- components/features/blog/ZoomImage.tsx
+++ components/features/blog/ZoomImage.tsx
@@ -9,21 +9,19 @@ type ZoomImageProps = Omit<ImageProps, 'alt'> & { alt: string };
 
 export function ZoomImage({ className, alt, ...props }: ZoomImageProps) {
   return (
-    <div className="my-6 flex justify-center">
-      <Zoom zoomMargin={16}>
-        <Image
-          alt={alt ?? ''}
-          {...props}
-          className={[
-            'h-auto max-w-full cursor-zoom-in rounded-lg',
-            'max-h-[60vh] object-contain',
-            'md:max-w-2xl',
-            className,
-          ]
-            .filter(Boolean)
-            .join(' ')}
-        />
-      </Zoom>
-    </div>
+    <Zoom zoomMargin={16}>
+      <Image
+        alt={alt ?? ''}
+        {...props}
+        className={[
+          'h-auto max-w-full cursor-zoom-in rounded-lg',
+          'max-h-[60vh] object-contain',
+          'md:max-w-2xl',
+          className,
+        ]
+          .filter(Boolean)
+          .join(' ')}
+      />
+    </Zoom>
   );
 }
